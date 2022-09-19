d6fa3aebaf1e375494dea59d52b3ae5a04a3f552
ChrisTseng <lionms200789@gmail.com>
2022-09-20 00:21:06 +0800

    add storybook and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{js,jsx}', './stories/**/*.{js,jsx}'],
+  mode: 'jit',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
